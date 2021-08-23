797d4efcd3b7ac5fd2154bddcd87cc682cfbe552
pygamer0 <maanasyt@gmail.com>
2021-08-23 16:03:02 +0530

    2dee: obs class

--- 2dee/2dee.py
+++ 2dee/2dee.py
@@ -3,6 +3,20 @@ import sys
 def eprint(*args, **kwargs):
     print(*args, **kwargs, file=sys.stderr)
 
+class Obstacle:
+    def __init__(self):
+        pass
+
+    def on_collide(self, mover):
+        if mover.direction == "DOWN":
+            mover.direction == "LEFT"
+        elif mover.direction == "UP":
+            mover.direction == "RIGHT"
+        elif mover.direction == "LEFT":
+            mover.direction == "DOWN"
+        elif mover.direction == "RIGHT":
+            mover.direction == "UP"
+
 class Mover:
     def __init__(self, x=0, y=0):
         self.pos = [x, y]
@@ -27,7 +41,7 @@ if not len(sys.argv) > 1:
     exit(1)
 
 try:
-    file= open(sys.argv[1])
+    file = open(sys.argv[1])
 except FileNotFoundError:
     eprint(f"File '{sys.argv[1]}' not found.")
     exit(2)
